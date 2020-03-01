3d48cb3d268969fe0d6223b17238d8039717646b
David Castillo <hola@davecastworks.com>
2020-03-01 00:50:45 -0400

    Initial dist subtree commit

---
+++ dist/js/chunk-2d0e5e97.acb084c6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is a 404 page")])])}],c=n("2877"),u={},i=Object(c["a"])(u,s,a,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0e5e97.acb084c6.js.map
